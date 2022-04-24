e813c95103491fb1b265817ad70b9a7310fa1ba6
12944qwerty <qwerty12944qwerty@gmail.com>
2022-04-23 21:32:48 -0500

    channel context menu

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const { Plugin } = require('powercord/entities');
-const { React, getModule, constants: { Routes, ActionTypes }, FluxDispatcher, contextMenu } = require('powercord/webpack');
+const { React, getModule, constants: { Routes, ActionTypes }, FluxDispatcher, getModuleByDisplayName } = require('powercord/webpack');
 const { inject, uninject } = require('powercord/injector');
-const { AsyncComponent, ContextMenu } = require('powercord/components');
+const { AsyncComponent } = require('powercord/components');
+const { wrapInHooks } = require('powercord/util');
 
 const Popout = AsyncComponent.fromDisplayName('Popout');
 const UserPopoutContainer = AsyncComponent.from(getModule(m => m.type?.displayName === 'UserPopoutContainer'));
 
 const { transitionTo } = getModule([ 'transitionTo' ], false);
 const channelStore = getModule([ 'getDMFromUserId', 'getChannel' ], false);
+const { openUserContextMenu } = getModule(['openUserContextMenu'], false);
+const userStore = getModule([ 'initialize', 'getCurrentUser' ], false);
+const { getChannel } = getModule([ 'initialize', 'hasChannel' ], false);
 
 module.exports = class ClickableMentions extends Plugin {
     startPlugin () {
@@ -51,37 +55,26 @@ module.exports = class ClickableMentions extends Plugin {
                     FluxDispatcher.dirtyDispatch({ type: ActionTypes.LAYER_POP });
                 
                     if (channelId) {
-                    const channel = channelStore.getChannel(channelId);
-                    guildId = channel.guild_id;
-                    messageId = channel.lastMessageId;
+                        const channel = channelStore.getChannel(channelId);
+                        guildId = channel.guild_id;
+                        messageId = channel.lastMessageId;
                     }
                 
                     transitionTo(Routes.CHANNEL(guildId, channelId, messageId));
                 },
-                onContextMenu: (e) => {
-                    try {
-                        let menu = React.createElement(ContextMenu, {
-                            itemGroups: [
-                                [{
-                                    type: 'button',
-                                    name: 'Mark as Read',
-                                    onClick: () => {console.log("mark as read")}
-                                }],
-                                [{
-                                    type: 'submenu',
-                                    name: 'Mute Channel',
-                                    getItems: () => {return [
-                                        {
-                                            type: 'button',
-                                            name: 'For 15 Minutes',
-                                            onClick: () => {console.log("mute 15 minutes")}
-                                        }
-                                    ]}
-                                }]
-                            ]
-                        });
-                        contextMenu.openContextMenu(e, () => menu) // temporary context menu
-                    } catch (err) {console.log(err)}
+                onContextMenu: (event) => {
+                    const channel = channelStore.getChannel(channelId);
+                    let ConnectedChannel;
+                    
+                    if (channel.isVocal()) {                    
+                        ConnectedChannel = getModuleByDisplayName('ConnectedVoiceChannel', false);
+                    } else {
+                        ConnectedChannel = getModuleByDisplayName('ConnectedTextChannel', false);
+                    }
+
+                    return (
+                        wrapInHooks(() => new ConnectedChannel({ channel, guild: {} }))()
+                    ).type.DecoratedComponent({ channel }).prototype.constructor.handleContextMenu(event);
                 },
             }, res)
 
@@ -93,10 +86,6 @@ module.exports = class ClickableMentions extends Plugin {
         if (!children.props) return children;
 
         children.props.className = [ children.props.className, this.classes.cursorPointer ].filter(Boolean).join(' ');
-
-        const { openUserContextMenu } = getModule(['openUserContextMenu'], false);
-        const userStore = getModule([ 'initialize', 'getCurrentUser' ], false);
-        const { getChannel } = getModule([ 'initialize', 'hasChannel' ], false);
 
         return React.createElement(Popout, {
             renderPopout: (popoutProps) => React.createElement(UserPopoutContainer, {
